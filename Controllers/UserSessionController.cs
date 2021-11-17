0fd02940d8b6f40d3c1e8a8099063352de66e8d4
bunikap <91877432+bunikap@users.noreply.github.com>
2021-11-17 19:08:15 +0700

    httpcontext mapvalue from user login

---
+++ Controllers/UserSessionController.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using qwerty.Data;
+using qwerty.Models;
+
+namespace qwerty.Controllers
+{
+    public class UserSessionController : Controller
+    {
+        private readonly QwertyContext _context;
+
+        public UserSessionController(QwertyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Permission
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("Id,own,DepartmentId,PermissionId,pswd")] Owner owner)
+        {
+
+            var findUser = _context.Owner.Where(s => s.own == owner.own).Where(s => s.pswd == owner.pswd);
+
+            if (findUser.Count() > 0)
+            {
+                var user = findUser.FirstOrDefault();
+                ViewBag.own = user;
+                HttpContext.Session.SetString("userName", user.own);
+                HttpContext.Session.SetInt32("userId", user.Id);
+                var test = HttpContext.Session.GetString("userName");
+                return RedirectToAction("Index", "Home");
+
+            }
+
+
+            return View();
+
+
+
+        }
+
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            var test = HttpContext.Session.GetString("userName");
+            return RedirectToAction("Index", "UserSession");
+        }
+
+
+    }
+}
