202e00e549d94a49d2631c560c4779e93a824196
bunikap <91877432+bunikap@users.noreply.github.com>
2021-11-17 19:09:00 +0700

    map new database

---
+++ Migrations/20211117063750_InitialCreate1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace qwerty.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "pswd",
+                table: "Owner",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "pswd",
+                table: "Owner");
+        }
+    }
+}
