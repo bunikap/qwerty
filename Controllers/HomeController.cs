089a2554353f79467bd20816b1a0fc2150998303
bunikap <91877432+bunikap@users.noreply.github.com>
2021-11-17 11:44:54 +0700

    Remove Unnecessary using and action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis;
 using qwerty.Models;
 using Microsoft.AspNetCore.Http;
-using System.Text;
 using System.IO;
 using System.Data;
 using System;
-using ExcelDataReader;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using NPOI.HSSF.UserModel;
@@ -35,7 +33,7 @@ namespace qwerty.Controllers
         public ActionResult Index()
         {
 
-            ViewData["OwnerId"] = new SelectList(_context.Owner.Where(s=>s.visible==1), "Id", "own");
+            ViewData["OwnerId"] = new SelectList(_context.Owner.Where(s => s.visible == 1), "Id", "own");
 
 
             return View();
@@ -201,124 +199,6 @@ namespace qwerty.Controllers
 
         }
 
-
-        public async Task<IActionResult> Export()
-
-        {
-
-            string sWebRootFolder = _webHostEnvironment.WebRootPath;
-
-            string sFileName = @"Employees.xlsx";
-
-            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
-
-            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
-
-            var memory = new MemoryStream();
-
-            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
-
-            {
-
-                IWorkbook workbook;
-
-                workbook = new XSSFWorkbook();
-
-                ISheet excelSheet = workbook.CreateSheet("employee");
-
-                IRow row = excelSheet.CreateRow(0);
-
-                row.CreateCell(0).SetCellValue("EmployeeId");
-
-                row.CreateCell(1).SetCellValue("EmployeeName");
-
-                row.CreateCell(2).SetCellValue("Age");
-
-                row.CreateCell(3).SetCellValue("Sex");
-
-                row.CreateCell(4).SetCellValue("Designation");
-
-                row = excelSheet.CreateRow(1);
-
-                row.CreateCell(0).SetCellValue(1);
-                row.CreateCell(1).SetCellValue("Jack Supreu");
-
-                row.CreateCell(2).SetCellValue(45);
-
-                row.CreateCell(3).SetCellValue("Male");
-
-                row.CreateCell(4).SetCellValue("Solution Architect");
-
-
-                row = excelSheet.CreateRow(2);
-
-                row.CreateCell(0).SetCellValue(2);
-
-                row.CreateCell(1).SetCellValue("Steve khan");
-
-                row.CreateCell(2).SetCellValue(33);
-
-                row.CreateCell(3).SetCellValue("Male");
-
-                row.CreateCell(4).SetCellValue("Software Engineer");
-
-
-                row = excelSheet.CreateRow(3);
-
-                row.CreateCell(0).SetCellValue(3);
-
-                row.CreateCell(1).SetCellValue("Romi gill");
-
-                row.CreateCell(2).SetCellValue(25);
-
-                row.CreateCell(3).SetCellValue("FeMale");
-
-                row.CreateCell(4).SetCellValue("Junior Consultant");
-
-
-                row = excelSheet.CreateRow(4);
-
-                row.CreateCell(0).SetCellValue(4);
-
-                row.CreateCell(1).SetCellValue("Hider Ali");
-
-                row.CreateCell(2).SetCellValue(34);
-
-                row.CreateCell(3).SetCellValue("Male");
-
-                row.CreateCell(4).SetCellValue("Accountant");
-
-
-                row = excelSheet.CreateRow(5);
-
-                row.CreateCell(0).SetCellValue(5);
-
-                row.CreateCell(1).SetCellValue("Mathew");
-
-                row.CreateCell(2).SetCellValue(48);
-
-                row.CreateCell(3).SetCellValue("Male");
-
-                row.CreateCell(4).SetCellValue("Human Resource");
-
-
-                workbook.Write(fs);
-
-            }
-
-            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
-
-            {
-
-                await stream.CopyToAsync(memory);
-
-            }
-
-            memory.Position = 0;
-
-            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
-
-        }
 
     }
 
