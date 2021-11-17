24a5493d11fcc7f73e98136df7142830d8b775fa
bunikap <91877432+bunikap@users.noreply.github.com>
2021-11-17 11:49:10 +0700

    set login page when starter

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace qwerty
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=UserSession}/{action=Index}/{id?}");
             });
         }
     }
